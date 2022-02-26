74b2a3851a7d8f76a64bdeeece898d8e264ed40b
ligarunge <liga.runge@gmail.com>
2022-02-26 17:30:28 +0200

    Content, Header

--- src/App.js
+++ src/App.js
@@ -1,19 +1,11 @@
-import { Route, Routes } from "react-router-dom";
+
+import Content from "./Components/Content";
 import Footer from "./Components/Footer";
-import Categories from "./Pages/Categories";
-import Login from "./Pages/Login";
-import Orders from "./Pages/Orders";
-import Products from "./Pages/Products";
 
 function App() {
   return (
     <div>
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/categories" element={<Categories />} />
-        <Route path="/products" element={<Products />} />
-        <Route path="/orders" element={<Orders />} />
-      </Routes>
+      <Content />
       <Footer />
     </div>
   );
--- src/Components/Content.js
+++ src/Components/Content.js
@@ -1,8 +1,19 @@
+import { Route, Routes } from "react-router-dom";
+import Categories from "../Pages/Categories";
+import Login from "../Pages/Login";
+import Orders from "../Pages/Orders";
+import Products from "../Pages/Products";
+
 function Content() {
 
     return (
         <div>
-
+            <Routes>
+                <Route path="/" element={<Login />} />
+                <Route path="/categories" element={<Categories />} />
+                <Route path="/products" element={<Products />} />
+                <Route path="/orders" element={<Orders />} />
+            </Routes>
         </div>
     )
 }
