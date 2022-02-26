7611d95eedec74c8fba7fe58e06ea6d7e89b1198
Monta Pecina <montapecina@gmail.com>
2022-02-26 20:01:01 +0100

    login update

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -2,9 +2,9 @@ function Footer() {
 
     return (
         
-        <div className="container">
-            <footer className="d-flex flex-wrap py-3 my-4 border-top">
-                <p className="col mb-0 ">© 2022 Accenture bootcamp hackathon-4, made by Maija, Līga, Monta </p>
+        <div className="container position-absolute bottom-0 start-0 end-0">
+            <footer className="py-3 my-4 border-top">
+                <p className="mb-0">© 2022 Accenture bootcamp hackathon-4, made by Maija, Līga, Monta </p>
             </footer>
         </div>
     )
