9893f4d95ef52db72617372d7f026791a3feb16c
ligarunge <liga.runge@gmail.com>
2022-02-26 09:34:59 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import { Route, Routes } from "react-router-dom";
+import Categories from "./Pages/Categories";
+import Login from "./Pages/Login";
+import Orders from "./Pages/Orders";
+import Products from "./Pages/Products";
+
+function App() {
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/categories" element={<Categories />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/orders" element={<Orders />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
