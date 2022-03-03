1e4dea636101f1b493145ad224b1597b1a9ce6af
ligarunge <liga.runge@gmail.com>
2022-03-03 11:29:55 +0200

    Login button

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -6,7 +6,7 @@ function Footer() {
             <footer className="border-top">
                 <div className="row mt-4">
                     <div className="col-md-6 text-white">
-                        <div className="row display-5">
+                        <div className="row display-6">
                             <div className="col-12 text-center fw-bolder">
                                 FIND US AT
                             </div>
@@ -23,7 +23,7 @@ function Footer() {
                         </div>
                     </div>
                     <div className="col-md-6 text-white">
-                        <div className="row display-5">
+                        <div className="row display-6">
                             <div className="col-12 text-center fw-bolder">
                                 CONTACT US
                             </div>
